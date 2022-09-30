faf259975b20970fb98d18a8f6a7c61b09db45c0
Sketchead <megamam_atxel@hotmail.com>
2022-09-29 19:56:51 -0600

    Added user bans

--- app/src/main/java/mx/tecnm/tepic/autocolimetro_app/MainActivity.kt
+++ app/src/main/java/mx/tecnm/tepic/autocolimetro_app/MainActivity.kt
@@ -1,37 +1,73 @@
 package mx.tecnm.tepic.autocolimetro_app
 
-import android.R
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.tecnm.tepic.autocolimetro_app.databinding.ActivityMainBinding
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private companion object{
+        private const val RC_SIGN_IN = 100
+        private const val TAG = "GOOGLE_SIGN_IN_TAG"
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            //  you will get result here in result.data
+            if(result.data!!.data.toString()=="EXCEDE"){
+                AlertDialog.Builder(this)
+                    .setTitle("ATENCIÓN")
+                    .setMessage("Cuenta bloqueada por exceso de alcohol")
+                    .show()
+            }
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val googleSignOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("612542666031-bmo0ips5p95oolml962p6hv7or7d0sdf.apps.googleusercontent.com")
             .requestEmail()
             .build()
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignOptions)
 
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        //Ya hay user iniciado
-        //val account = GoogleSignIn.getLastSignedInAccount(this)
-        //updateUI(account)
-        //}binding.signInButton.setSize(SignInButton.SIZE_STANDARD)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.signInButton.setOnClickListener {
+            val intent = googleSignInClient.signInIntent
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+
         binding.register.setOnClickListener {
             if(binding.mail.text.isEmpty() || binding.pass.text.isEmpty()){
                 Toast.makeText(this, "Favor de llenar los campos", Toast.LENGTH_LONG).show()
@@ -63,6 +99,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
         }
+
         binding.signin.setOnClickListener {
             if(binding.mail.text.isEmpty() || binding.pass.text.isEmpty()){
                 Toast.makeText(this, "Favor de llenar los campos", Toast.LENGTH_LONG).show()
@@ -90,8 +127,89 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
     private fun invocarOtraVentana() {
-        startActivity(Intent(this,MainActivity2::class.java))
-        finish()
+        startForResult.launch(Intent(this,MainActivity2::class.java))
     }
+//this,MainActivity2::class.java
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                //Logeado
+                val account = accountTask.getResult(ApiException::class.java)
+                FirebaseAuthwithGoogle(account)
+
+            }catch(e: Exception){
+                AlertDialog.Builder(this)
+                    .setMessage("Error onresult"+e.message+e.localizedMessage)
+                    .show()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun FirebaseAuthwithGoogle(account: GoogleSignInAccount?) {
+        val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener {
+                //Success
+                val firebaseUser = firebaseAuth.currentUser
+                val uid = firebaseAuth.uid
+                val email = firebaseUser!!.email
+                Log.d(TAG,"mail: $email")
+                Log.d(TAG,"uid $uid")
+
+                //check if new user
+                if(it.additionalUserInfo!!.isNewUser){
+                    Log.d(TAG,"Registrado")
+                    Toast.makeText(this@MainActivity, "Registrado",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this@MainActivity, "Sesión iniciada",Toast.LENGTH_LONG).show()
+                }
+                val bd = FirebaseFirestore.getInstance()
+                val current = Calendar.getInstance()
+                val day=current.get(Calendar.DAY_OF_MONTH)
+                val month=current.get(Calendar.MONTH)+1
+                val year=current.get(Calendar.YEAR)
+                var banned = false
+                bd.collection(FirebaseAuth.getInstance().uid+"")
+                    .get()
+                    .addOnSuccessListener { document ->
+                        for (documento in document!!){
+                            val fecha = documento.getTimestamp("fecha")
+                            //val c = Calendar.getInstance()
+                            Log.d("TIEMPO SEGS","${fecha!!.seconds}")
+                            val test_timestamp = fecha!!.seconds*1000
+                            val triggerTime = LocalDateTime.ofInstant(
+                                Instant.ofEpochMilli(test_timestamp),
+                                TimeZone.getDefault().toZoneId()
+                            )
+                            //triggerTime.dayOfMonth==day && triggerTime.monthValue==(month) && triggerTime.year==year
+                            //Toast.makeText(this@MainActivity, "Entra ${triggerTime.dayOfMonth==day && triggerTime.monthValue==(month) && triggerTime.year==year} ",Toast.LENGTH_LONG).show()
+                            if(triggerTime.dayOfMonth==day && triggerTime.monthValue==(month) && triggerTime.year==year ){
+                                Toast.makeText(this@MainActivity, "baneado",Toast.LENGTH_LONG).show()
+                                banned = true
+                            }
+                        }//for
+                        if (banned){
+                            AlertDialog.Builder(this)
+                                .setTitle("ATENCIÓN")
+                                .setMessage("Cuenta bloqueada por exceso de alcohol.\n")
+                                .show()
+                            FirebaseAuth.getInstance().signOut();
+                        }else{
+                            invocarOtraVentana()
+                        }
+                    }
+            }
+            .addOnFailureListener {
+                AlertDialog.Builder(this@MainActivity)
+                    .setMessage("Error en authwiuthgoogle"+it.message+"\n"+it.localizedMessage)
+                    .show()
+            }
+    }
+
 }
--- app/src/main/java/mx/tecnm/tepic/autocolimetro_app/MainActivity2.kt
+++ app/src/main/java/mx/tecnm/tepic/autocolimetro_app/MainActivity2.kt
@@ -2,17 +2,28 @@ package mx.tecnm.tepic.autocolimetro_app
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.widget.doOnTextChanged
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.tecnm.tepic.autocolimetro_app.databinding.ActivityMain2Binding
+import java.sql.Timestamp
+import java.time.LocalDate
+
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var binding: ActivityMain2Binding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMain2Binding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -40,13 +51,37 @@ class MainActivity2 : AppCompatActivity() {
         binding.gradoAl.doOnTextChanged { text, start, before, count ->
             if(text!!.isEmpty()) return@doOnTextChanged
             val ac = text.toString().toFloat()
-            if(ac>4){
+            if(ac>=4){
                 Toast.makeText(this, "Grado de alcohol sobrepasado: "+ac, Toast.LENGTH_LONG).show()
 
                 with(NotificationManagerCompat.from(this)) {
                     // notificationId is a unique int for each notification that you must define
                     notify(1, builder.build())
+                    val current = LocalDate.now()
+                    val year=current.year
+                    val month=current.month.value
+                    val day=current.dayOfMonth
+                    val datos = hashMapOf(
+                        "fecha" to Timestamp((year - 1900), (month - 1), (day), 0, 0, 0, 0),
+                    )
+                    val bd = FirebaseFirestore.getInstance()
+                    bd.collection(FirebaseAuth.getInstance().uid+"")
+                        .add(datos)
+                        .addOnSuccessListener {
+                            Log.d("REPORTE","Insertado")
+                        }
+                        .addOnFailureListener {
+                            AlertDialog.Builder(this@MainActivity2)
+                                .setTitle("Error")
+                                .setMessage(it.message)
+                                .show()
+                        }
                 }
+                val data = Intent()
+                data.setData(Uri.parse("EXCEDE"));
+                setResult(RESULT_OK, data);
+                FirebaseAuth.getInstance().signOut();
+                finish()
             }
         }
     }
